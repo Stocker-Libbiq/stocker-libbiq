5461d3c4e570d9dc564bd351af731379f96a83dd
Keiler Guardo <keilerguardo@gmail.com>
2022-09-06 18:42:39 -0500

    AppBar Added

--- lib/pages/dashboard.dart
+++ lib/pages/dashboard.dart
@@ -5,33 +5,52 @@ class Dashboard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Row(
-          children: [
-            Container(
-              color: Colors.blue,
-              width: MediaQuery.of(context).size.width / 5,
-              height: MediaQuery.of(context).size.height / 1.035,
-            ),
-            Expanded(
-              child: Container(
-                color: Colors.red,
-                width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.height / 1.035,
-              ),
-            ),
-          ],
-        ),
-        Container(
-          color: Colors.amber,
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height / 30,
-          child: const Text('Stocker Libbiq V1.0.0', 
-            style: TextStyle(fontSize: 10.0)
+    return MaterialApp(
+      title: 'Material App',
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Material App Bar'),
           ),
-        ),
-      ],
+          body: Column(
+            children: [
+              Row(
+                children: [
+                  Container(
+                    color: Colors.blue,
+                    width: MediaQuery.of(context).size.width / 5,
+                    height: MediaQuery.of(context).size.height / 1.11,
+                    child: Column(
+                      children: [
+                        Row(
+                          children: const [
+                            Text('Inicio'),
+                            Icon(Icons.home)
+                          ],
+                        ),
+                        Text('Reactivos'),
+                        Text('Stock'),
+                        Text('Estadisticas'),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      color: Colors.red,
+                      width: MediaQuery.of(context).size.width,
+                      height: MediaQuery.of(context).size.height / 1.11,
+                    ),
+                  ),
+                ],
+              ),
+              Container(
+                color: Colors.amber,
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height / 30,
+                child: const Text('Stocker Libbiq V1.0.0',
+                    style: TextStyle(fontSize: 10.0)),
+              ),
+            ],
+          )),
     );
   }
 }
