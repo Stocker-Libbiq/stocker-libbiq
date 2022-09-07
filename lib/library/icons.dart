9e26d454943d6d400e3cf8c8e0a7b7c16611b0df
Soad Freyle <soadfre@gmail.com>
2022-09-07 00:41:46 -0500

    adding icons

---
+++ lib/library/icons.dart
@@ -0,0 +1,21 @@
+class StockerIcons {
+  static String google = 'assets/icons/google.png';
+  static String arrowBack = 'assets/icons/arrow_back.svg';
+  static String houseFillo = 'assets/icons/house_FILLO.svg';
+  static String accountCircle = 'assest/icons/account_circle.svg';
+  static String close = 'assets/icons/close.svg';
+  static String search = 'assets/icons/search.svg';
+  static String logout = 'assets/icons/logout.svg';
+  static String locationOn = 'assets/icons/location_on.svg';
+  static String addCircle = 'assets/icons/add_circle.svg';
+  static String info = 'assets/icons/info.svg';
+  static String arrowCircleDown = 'assets/icons/arrow_circle_down.svg';
+  static String arrowCircleUp = 'assets/icons/arrow_circle_up.svg';
+  static String science = 'assets/icons/science.svg';
+  static String swapHoriz = 'assets/icons/swap_horiz.svg';
+  static String viewList = 'assets/icons/view_list.svg';
+  static String emergency = 'assets/icons/emergency.svg';
+  static String verifiedUser = 'assets/icons/verified_user.svg';
+  static String error = 'assets/icons/error.svg';
+  static String warning = 'assets/icons/warning.svg';
+}
