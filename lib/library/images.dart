4bde5295a16b316028775f02d46e80ccb1da668f
Soad Freyle <soadfre@gmail.com>
2022-09-08 21:21:13 -0500

    lines of code deleted

--- lib/library/images.dart
+++ lib/library/images.dart
@@ -1,9 +1,4 @@
 class StockerImages {
   static String imageLIBBIQ = 'assets/images/imageLIBBIQ.png';
-  static String nombreAppConLab = 'assets/images/imageNombreAppConLab.png';
-  static String controla = 'assets/images/loginBrowserControla.png';
-  static String gestiona = 'assets/images/loginBrowerGestiona.png';
-  static String ingresaGoogle = 'assets/images/loginBrowserIngresaGoogle.png';
-  static String optimiza = 'assets/images/loginBrowersOptimiza.png';
-  static String planifica = 'assets/images/loginBrowserPlanifica.png';
+  static String appLab = 'assets/images/imageNombreAppConLab.png';
 }
